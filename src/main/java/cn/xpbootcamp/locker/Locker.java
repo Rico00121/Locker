cf7fec196e19dd2c423c9ccf10b46fb1fa0f638d
Ruikang Tao <ruikang.tao@thoughtworks.com>
2022-07-20 11:30:11 +0800

    refactor: add a field availableCapacity

--- src/main/java/cn/xpbootcamp/locker/Locker.java
+++ src/main/java/cn/xpbootcamp/locker/Locker.java
@@ -3,20 +3,22 @@ package cn.xpbootcamp.locker;
 import java.util.HashMap;
 
 public class Locker {
-    private int capacity;
+    private final int capacity;
+    private int availableCapacity;
     private final HashMap<Ticket, Bag> bags = new HashMap<>();
 
     public Locker(int capacity) {
         this.capacity = capacity;
+        this.availableCapacity = capacity;
     }
 
     public Ticket save(Bag bag) {
-        if (capacity == 0){
+        if (availableCapacity == 0){
             throw new LockerIsFullException();
         }
         Ticket ticket = new Ticket();
         bags.put(ticket,bag);
-        capacity--;
+        availableCapacity--;
         return ticket;
     }
 
