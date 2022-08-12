cd25613eb1887772f163452f55a0ec84bb7c8434
Ruikang Tao <ruikang.tao@thoughtworks.com>
2022-08-12 16:45:24 +0800

    refactor: extract constant for test

--- src/test/java/cn/xpbootcamp/locker/PrimaryLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker/PrimaryLockerRobotTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PrimaryLockerRobotTest {
+
+    public static final int CAPACITY = 2;
+
     @Test
     public void should_return_ticket_and_save_in_first_locker_when_all_lockers_have_available_capacity() {
-        Locker locker1 = new Locker(2);
-        Locker locker2 = new Locker(2);
+        Locker locker1 = new Locker(CAPACITY);
+        Locker locker2 = new Locker(CAPACITY);
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(
                 Lists.newArrayList(locker1, locker2));
 
@@ -17,4 +20,5 @@ public class PrimaryLockerRobotTest {
         Assertions.assertNotNull(ticket);
         Assertions.assertEquals(locker1.getAvailableCapacity(), 1);
     }
+
 }
