cd71592228997bfc1022314f64a27980025c4055
Ruikang Tao <ruikang.tao@thoughtworks.com>
2022-08-15 16:28:42 +0800

    feat: smart locker robot save in first locker when all lockers available

---
+++ src/test/java/cn/xpbootcamp/locker/SmartLockerRobotTest.java
@@ -0,0 +1,20 @@
+package cn.xpbootcamp.locker;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SmartLockerRobotTest {
+    @Test
+    public void should_saved_to_more_capacity_locker_when_first_locker_is_3_and_second_is_2_when_smart_robot_save_bag() {
+        Locker firstLocker = new Locker(3);
+        Locker secondLocker = new Locker(2);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Lists.newArrayList(firstLocker, secondLocker));
+        Bag bag = new Bag();
+
+        Ticket ticket = smartLockerRobot.save(bag);
+
+        Assertions.assertTrue(firstLocker.contains(ticket));
+
+    }
+}
